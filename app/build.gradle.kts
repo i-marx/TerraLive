a09cc589fae530e30588ee46ca766063500fe4c2
i-marx <marx@hotmail.lt>
2026-09-15 20:11:07 +0300

    versionCode 63

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
         applicationId = "com.terralive.wallpapers"
         minSdk = 26
         targetSdk = 36
-        versionCode = 62
+        versionCode = 63
         versionName = "1.0"
     }
     signingConfigs {
