81f7443b23972901b78271f5af2e508b16a4a98b
i-marx <marx@hotmail.lt>
2026-09-26 21:07:27 +0300

    vc108: Play in-app review dependency

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.terralive.wallpapers"
         minSdk = 26
         targetSdk = 36
-        versionCode = 107
-        versionName = "1.0.107"
+        versionCode = 108
+        versionName = "1.0.108"
     }
     signingConfigs {
         create("release") {
@@ -53,4 +53,5 @@ dependencies {
     implementation("androidx.core:core-splashscreen:1.0.1")
     implementation("androidx.appcompat:appcompat:1.7.0")
     implementation("com.google.android.material:material:1.12.0")
+    implementation("com.google.android.play:review:2.0.2")
 }
